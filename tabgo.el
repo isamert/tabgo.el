2556e1f8db6ebe913d1be1552d2fc966e81adde3
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2023-04-09 21:53:41 +0200

    Remove unnecessary sharp-quotes

--- tabgo.el
+++ tabgo.el
@@ -86,11 +86,11 @@ TYPE can be either \\='line or \\='tab."
   `(let* ((tabgo-tab-line-map (make-hash-table :test #'equal))
           (old-tab-line-fn tab-line-tab-name-format-function)
           (tab-line-tab-name-format-function
-           #'(lambda (tab tabs)
-               (let ((key (tabgo--get-key 'line (seq-position tabs 'dummy (lambda (it _) (equal tab it)))))
-                     (old-format (funcall old-tab-line-fn tab tabs)))
-                 (map-put! tabgo-tab-line-map (substring-no-properties key) tab)
-                 (format "%s%s" key (substring old-format 1))))))
+           (lambda (tab tabs)
+             (let ((key (tabgo--get-key 'line (seq-position tabs 'dummy (lambda (it _) (equal tab it)))))
+                   (old-format (funcall old-tab-line-fn tab tabs)))
+               (map-put! tabgo-tab-line-map (substring-no-properties key) tab)
+               (format "%s%s" key (substring old-format 1))))))
      (set-window-parameter nil 'tab-line-cache nil)
      (force-mode-line-update t)
      ,@forms
@@ -103,31 +103,31 @@ TYPE can be either \\='line or \\='tab."
   `(let* ((tabgo-tab-bar-map (make-hash-table :test #'equal))
           (old-tab-bar-fn tab-bar-tab-name-format-function)
           (tab-bar-tab-name-format-function
-           #'(lambda (tab i)
-               (let* ((key (tabgo--get-key 'bar (1- i)))
-                      (old-format (funcall old-tab-bar-fn tab i))
-                      (new-format (format "%s%s"
-                                          key
-                                          (substring old-format 1))))
-                 (map-put! tabgo-tab-bar-map (substring-no-properties key) i)
-                 (if (string= old-format new-format)
-                     ;; HACK If new-format and old-format equal to
-                     ;; each other string-wise, even if the text
-                     ;; properties are different, tab-bar is not
-                     ;; updated with new text properties. So we do
-                     ;; some changes the original text.
-                     (let ((placeholder "~"))
-                       ;; If the old-format has the X button at the
-                       ;; end, the resulting new-format will look
-                       ;; exactly the same (except for the leading
-                       ;; char's face). Otherwise they'll see a "~" at
-                       ;; the end of their tab name, instead of what
-                       ;; it's supposed to be.
-                       (add-text-properties 0 1 (text-properties-at (1- (length new-format)) new-format) placeholder)
-                       (format "%s%s"
-                               (substring new-format 0 (1- (length new-format)))
-                               placeholder))
-                   new-format)))))
+           (lambda (tab i)
+             (let* ((key (tabgo--get-key 'bar (1- i)))
+                    (old-format (funcall old-tab-bar-fn tab i))
+                    (new-format (format "%s%s"
+                                        key
+                                        (substring old-format 1))))
+               (map-put! tabgo-tab-bar-map (substring-no-properties key) i)
+               (if (string= old-format new-format)
+                   ;; HACK If new-format and old-format equal to
+                   ;; each other string-wise, even if the text
+                   ;; properties are different, tab-bar is not
+                   ;; updated with new text properties. So we do
+                   ;; some changes the original text.
+                   (let ((placeholder "~"))
+                     ;; If the old-format has the X button at the
+                     ;; end, the resulting new-format will look
+                     ;; exactly the same (except for the leading
+                     ;; char's face). Otherwise they'll see a "~" at
+                     ;; the end of their tab name, instead of what
+                     ;; it's supposed to be.
+                     (add-text-properties 0 1 (text-properties-at (1- (length new-format)) new-format) placeholder)
+                     (format "%s%s"
+                             (substring new-format 0 (1- (length new-format)))
+                             placeholder))
+                 new-format)))))
      (force-mode-line-update t)
      ,@forms
      (force-mode-line-update t)))
